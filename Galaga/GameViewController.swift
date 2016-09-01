470e3952836a1d1463d42792b7370eb6c2c7e1b2
Jack Hamilton <jackham800@gmail.com>
2016-09-01 17:06:25 -0400

    Update 1.0

--- Galaga/GameViewController.swift
+++ Galaga/GameViewController.swift
@@ -1,52 +1,23 @@
-//
-//  GameViewController.swift
-//  Galaga
-//
-//  Created by App Camp on 7/25/16.
-//  Copyright (c) 2016 App Camp. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
+    var skView : SKView = SKView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        let scene = StartGameState(size: view.bounds.size)
+        scene.parentCtrl = self
+        skView = view as! SKView
+        skView.showsFPS = false
+        skView.showsNodeCount = false
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        scene.parentView = skView
+        skView.presentScene(scene)
     }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
