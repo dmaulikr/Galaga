db73daa51e755e30ff5896cf5fde529cf986ca64
Jack Hamilton <jackham800@gmail.com>
2017-07-06 10:07:12 -0400

    Updated test project, started building with new assets

---
+++ Nebula 'Nnihilation/Nebula 'NnihilationUITests/Nebula__NnihilationUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Nebula__NnihilationUITests.swift
+//  Nebula 'NnihilationUITests
+//
+//  Created by Jack Hamilton on 7/6/17.
+//  Copyright © 2017 App Camp. All rights reserved.
+//
+
+import XCTest
+
+class Nebula__NnihilationUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
