954caec5ba9b301cd2ad697bb64fd9724c12d11d
Jack Hamilton <jackham800@gmail.com>
2017-07-12 10:37:00 -0400

    Started using protocols for enemies

---
+++ Nebula 'Nnihilation/Nebula 'Nnihilation/Enemy.swift
@@ -0,0 +1,17 @@
+//
+//  Enemy.swift
+//  Nebula 'Nnihilation
+//
+//  Created by Jack Hamilton on 7/12/17.
+//  Copyright © 2017 App Camp. All rights reserved.
+//
+
+import Foundation
+
+protocol Enemy {
+    
+    var health: Int { get }
+    
+    var scoreValue: Int { get set }
+    
+}
